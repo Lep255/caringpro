5afffa6f04185c03a0789297cf9fe4dc13d299e5
Lep255 <106032433+Lep255@users.noreply.github.com>
2025-01-02 14:08:47 -0500

    Update Sticky Patient Info & Action.user.js

--- Sticky Patient Info & Action.user.js
+++ Sticky Patient Info & Action.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sticky Patient Info and Actions
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      5.0
 // @description  Make the patient info and actions sections sticky on scroll.
 // @author       You
 // @match        https://caringpro.inmyteam.com/*
