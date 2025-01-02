7b9fa96bfb32df7623f2e72270edc842378f849b
Lep255 <106032433+Lep255@users.noreply.github.com>
2025-01-02 14:46:00 -0500

    Update Show Notes Checkbox.user.js

--- Show Notes Checkbox.user.js
+++ Show Notes Checkbox.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Show Notes Checkbox
 // @namespace    http://tampermonkey.net/
-// @version      5.1
+// @version      5.2
 // @description  Automatically check the show notes checkbox on aide schedules
 // @author       You
 // @match        https://caringpro.inmyteam.com/*
